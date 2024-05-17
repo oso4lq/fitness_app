ac37c6ed2a16907bb09e1d08fffb03a2d99cf395
Michael Zorin <michael.zorin.rd@gmail.com>
2024-05-17 13:40:48 +0200

    classNameS, header component

---
+++ src/components/CourseMain/CourseMain.tsx
@@ -0,0 +1,14 @@
+// import classNames from "classnames";
+import styles from "./Bar.module.css";
+
+export default function CourseMain() {
+    // logic
+
+
+    return (
+        <>
+            <p>course main works!</p>
+            <h1>Подойдёт для вас, если:</h1>
+        </>
+    )
+}
