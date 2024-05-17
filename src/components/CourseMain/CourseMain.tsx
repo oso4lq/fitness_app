db8af2372631091b843aadd8e51fd67678792a24
Michael Zorin <michael.zorin.rd@gmail.com>
2024-05-17 18:46:12 +0200

    course styles

--- src/components/CourseMain/CourseMain.tsx
+++ src/components/CourseMain/CourseMain.tsx
@@ -16,16 +16,19 @@ export default function CourseMain({ courseData }: CourseMainProps) {
     return (
         <div className={styles.courseMainBox}>
             <Activity courseData={courseData} />
-            <h2>Подойдёт для вас, если:</h2>
-            <div>
+
+            <h2 className={styles.title}>Подойдёт для вас, если:</h2>
+
+            <div className={styles.fitting}>
                 {fitting.map((fittingItem: any, index: any) => (
-                    <div key={index}>{fittingItem}</div>
+                    <div className={styles.fittingItem} key={index}>{fittingItem}</div>
                 ))}
             </div>
-            <h2>Направления</h2>
-            <div>
+
+            <h2 className={styles.title}>Направления</h2>
+            <div className={styles.direction}>
                 {directions.map((direction: any, index: any) => (
-                    <div key={index}>{direction}</div>
+                    <div className={styles.directionItem} key={index}>{direction}</div>
                 ))}
             </div>
         </div>
