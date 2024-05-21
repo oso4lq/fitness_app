62de0564b0d67dcb7e45f6a482aa2cb5312a90e4
Michael Zorin <michael.zorin.rd@gmail.com>
2024-05-21 19:56:31 +0200

    header now can be disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-// "use client"
-
 import Link from "next/link";
-
 
 export default function Home() {
   return (
     <>
-
-
       <div className="DEVBLOCK">
         <p>блок на время разработки с кнопками на страницы</p>
-        <Link href={"/course/"}>Course Page</Link>
+        <Link href={"/course/"}>Course Page q02a6i</Link>
         <Link href={"/profile/"}>Profile Page</Link>
-        <Link href={"/workout/"}>Workout Page</Link>
+        <Link href={"/workout/"}>Workout Page 9yolz2</Link>
       </div>
     </>
   );
