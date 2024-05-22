2930f8de29d60ea1918edc368becfbc385779dcf
svet-nikol <plaksina.sn@mail.ru>
2024-05-23 00:32:19 +0300

    ProfileCourseList component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import ProfileCourseListBlock from "@/components/ProfileCourseListBlock/ProfileCourseListBlock";
 import data from "../../lib/data.json";
 import ProfileUserInfoBlock from "@/components/ProfileUserInfoBlock/ProfileUserInfoBlock";
 
@@ -5,6 +6,7 @@ export default function ProfilePage() {
   return (
     <div data-tid="pageWrap" className="px-[100px] py-[60px]">
       <ProfileUserInfoBlock />
+      <ProfileCourseListBlock/>
     </div>
   );
 }
---
+++ src/components/ProfileCourseListBlock/ProfileCourseListBlock.tsx
@@ -0,0 +1,10 @@
+export default function ProfileCourseListBlock() {
+  return (
+    <h2
+      data-tid="titleCourses"
+      className="mt-50 text-[40px] font-semibold leading-[44px]"
+    >
+      Мои курсы
+    </h2>
+  );
+}
