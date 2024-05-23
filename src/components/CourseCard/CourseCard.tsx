79d1e50790d7d46ecb2d22fa4885e59543174582
svet-nikol <plaksina.sn@mail.ru>
2024-05-23 09:39:29 +0300

    добавила иконки для уровня сложности

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { ProgressBar } from "../ProgressBar/ProgressBar";
 import theme_yoga from "../../../public/img/theme_yoga.png";
+import order_5 from "../../../public/img/icon/order_5.svg";
 
 export default function CourseCard() {
   return (
@@ -8,6 +9,7 @@ export default function CourseCard() {
       <div className="rounded-[30px] overflow-hidden">
         <Image src={theme_yoga} alt="theme_yoga" height={325} width={360} />
       </div>
+      <Image src={order_5} alt="order_5" height={18} width={18}/>
       <ProgressBar completionPercentage={4} />
     </div>
   );
