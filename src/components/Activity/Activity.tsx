12e341903611df2fe23c547c8dc06b95117917da
svet-nikol <plaksina.sn@mail.ru>
2024-05-27 01:29:59 +0300

    Activity и Заголовки стилизованы с tailwind css

--- src/components/Activity/Activity.tsx
+++ src/components/Activity/Activity.tsx
@@ -3,22 +3,25 @@ import styles from "./Activity.module.css";
 import Image from "next/image";
 
 export default function Activity({ courseData }: any) {
-    // logic
+  // logic
 
-    const { nameRU, nameEN } = courseData;
+  const { nameRU, nameEN } = courseData;
 
-
-    return (
-        <div className={styles.activityBox}>
-            <h1>{nameRU}</h1>
-            <Image
-                width={250}
-                height={150}
-                className={styles.activityImg}
-                src={`/img/${nameEN}.jpg`}
-                alt={`${nameRU}`}
-            />
-        </div>
-
-    )
+  return (
+    <div
+      data-tid="activityBox"
+      className="mb-[60px] rounded-[30px] flex relative" //justify-between
+    >
+      <h1 className="absolute mx-[40px] mt-[40px] text-[60px] font-medium leadin-[66px] text-white-base">{nameRU}</h1>
+      <div className="">
+        <Image
+          width={1160}
+          height={310}
+          className=""
+          src={`/img/themeBig_${nameEN}.png`}
+          alt={`${nameRU}`}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/CourseMain/CourseMain.tsx
+++ src/components/CourseMain/CourseMain.tsx
@@ -3,34 +3,43 @@ import styles from "./CourseMain.module.css";
 import { Activity } from "@/components/Activity";
 
 interface CourseMainProps {
-    courseData: any;
+  courseData: any;
 }
 
 export default function CourseMain({ courseData }: CourseMainProps) {
-    if (!courseData) {
-        return <div>no data loaded</div>;
-    }
+  if (!courseData) {
+    return <div>no data loaded</div>;
+  }
 
-    const { fitting, directions } = courseData;
+  const { fitting, directions } = courseData;
 
-    return (
-        <div className={styles.courseMainBox}>
-            <Activity courseData={courseData} />
+  return (
+    <div data-tid="courseMainBox" className="mt-[60px]">
+      <Activity courseData={courseData} />
 
-            <h2 className={styles.title}>Подойдёт для вас, если:</h2>
+      <h2 className="mb-[40px] text-[40px] font-semibold leadin-[44px] text-black-base">
+        Подойдёт для вас, если:
+      </h2>
 
-            <div className={styles.fitting}>
-                {fitting.map((fittingItem: any, index: any) => (
-                    <div className={styles.fittingItem} key={index}>{fittingItem}</div>
-                ))}
+      <div className={styles.fitting}>
+        {fitting.map((fittingItem: any, index: any) => (
+          <div key={index} className="flex gap-[25px]">
+            <div className="">{index + 1}</div>
+            <div className={styles.fittingItem}>
+              {fittingItem}
             </div>
+          </div>
+        ))}
+      </div>
 
-            <h2 className={styles.title}>Направления</h2>
-            <div className={styles.direction}>
-                {directions.map((direction: any, index: any) => (
-                    <div className={styles.directionItem} key={index}>{direction}</div>
-                ))}
-            </div>
-        </div>
-    );
+      <h2 className="mb-[40px] text-[40px] font-semibold leadin-[44px] text-black-base">Направления</h2>
+      <div className={styles.direction}>
+        {directions.map((direction: any, index: any) => (
+          <div className={styles.directionItem} key={index}>
+            {direction}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
