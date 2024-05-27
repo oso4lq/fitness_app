828f4128a8b2221bc40d6ffe6b81ffed0304b4a1
svet-nikol <plaksina.sn@mail.ru>
2024-05-27 14:41:07 +0300

    description-list в CourseMain стилизован с tailwind css

--- src/components/CourseBegin/CourseBegin.tsx
+++ src/components/CourseBegin/CourseBegin.tsx
@@ -10,14 +10,18 @@ export default function CourseBegin() {
     return (
         <div className={styles.beginBox}>
             <div className="w-[437px]">
-                <h1>Начните путь к новому телу</h1>
-                <p>проработка всех групп мышц    </p>
-                <p>тренировка суставов</p>
-                <p>улучшение циркуляции крови</p>
-                <p>упражнения заряжают бодростью</p>
-                <p>помогают противостоять стрессам</p>
-                <Button>Войдите, чтобы добавить курс</Button>
+                <h1 className="mb-[28px] text-black-base text-[60px] font-medium leading-[60px]">Начните путь к новому телу</h1>
+                <ul data-tid="description-list" className="mb-[28px] list-inside space-y-3">
+                    <li className="list-item-grey">проработка всех групп мышц</li>
+                    <li className="list-item-grey">тренировка суставов</li>
+                    <li className="list-item-grey">улучшение циркуляции крови</li>
+                    <li className="list-item-grey">упражнения заряжают бодростью</li>
+                    <li className="list-item-grey">помогают противостоять стрессам</li>
+                </ul>
+
                 {/* <button>Добавить курс</button> // заменить на условное применение текста в кнопке по авторизации*/}
+                <Button>Войдите, чтобы добавить курс</Button>
+
             </div>
             <Image
                 width={250}
@@ -27,5 +31,10 @@ export default function CourseBegin() {
                 alt="runner"
             />
         </div>
+
+        // <style>
+        //     .list-item {
+        //     }
+        // </style>
     )
 }
