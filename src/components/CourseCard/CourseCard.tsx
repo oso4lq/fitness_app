307599795c9d03000a87de3b40ff3daa544f5def
svet-nikol <plaksina.sn@mail.ru>
2024-06-05 11:15:03 +0300

    CourseCard component: added course details layout, added routes for CoursePage opening and SelectWorkoutPage opening

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -38,8 +38,8 @@ export default function CourseCard({ courseData, isPicked }: CourseCardProps) {
   const minusBtnUrl = "img/icon/minus.svg";
   const plusBtnUrl = "img/icon/plus.svg";
   const tooltipContent = isPicked
-    ? "Удалить тренировку"
-    : "Добавить тренировку";
+    ? "Удалить курс"
+    : "Добавить курс";
 
   const handleCoursePageOpen = () => {
     router.push(Routes.Course + "/" + courseData._id);
@@ -85,7 +85,47 @@ export default function CourseCard({ courseData, isPicked }: CourseCardProps) {
         <h3 className="text-[32px] leading-[35px] mb-[20px] font-medium">
           {courseData.nameRU}
         </h3>
-        {/* Other course details */}
+        <div className="flex start gap-[6px] flex-wrap mb-[20px]">
+          <div className="rounded-[50px] flex p-[10px] gap-[6px] bg-gray-light">
+            <Image
+              src="img/icon/calendar.svg"
+              width={18}
+              height={18}
+              alt="calendar"
+            />
+            <p className="text-[16px] font-normal leading-[17.6px]">25 дней</p>
+          </div>
+          <div className="rounded-[50px] flex p-[10px] gap-[6px] bg-gray-light">
+            <Image src="img/icon/time.svg" width={18} height={18} alt="time" />
+            <p className="text-[16px] font-normal leading-[17.6px]">
+              20-50 мин/день
+            </p>
+          </div>
+          <div className="rounded-[50px] flex p-[10px] gap-[6px] bg-gray-light">
+            <Image
+              src={`/img/icon/order_${courseData.order}.svg`}
+              alt={`order_${courseData.order}.svg`}
+              height={18}
+              width={18}
+            />
+            <p className="text-[16px] font-normal leading-[17.6px]">
+              Сложность
+            </p>
+          </div>
+        </div>
+        {isNotHomePage ? (
+          <>
+            {" "}
+            {/* Написать логику получения данных о проценте выполнении */}
+            <div className="mb-[40px]">
+              <p>{`Прогресс ${completionPercentage}%`}</p>
+              <ProgressBar completionPercentage={completionPercentage} />
+            </div>
+            <Button onClick={handleSelectWorkoutPageOpen}>{getTextButtonProfile(completionPercentage)}</Button>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
